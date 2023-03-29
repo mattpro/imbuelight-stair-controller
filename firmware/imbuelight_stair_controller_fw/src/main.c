c52dd192716cd64fc0b093b430c027e8197ef61e
Mateusz Gancarczyk <gancarczyk.mateusz@gmail.com>
2023-03-30 00:00:49 +0200

    Update PWM to 4 channel.

--- firmware/imbuelight_stair_controller_fw/src/main.c
+++ firmware/imbuelight_stair_controller_fw/src/main.c
@@ -18,12 +18,19 @@ void pio_pwm_set_level(PIO pio, uint sm, uint32_t level) {
     pio_sm_put_blocking(pio, sm, level);
 }
 
+#define PIO_1_STATE_MACHINE_1   0
+#define PIO_1_STATE_MACHINE_2   1
+#define PIO_1_STATE_MACHINE_3   2
+#define PIO_1_STATE_MACHINE_4   3
 
 
 int main() 
 {
-    const uint LED_PIN = 2;
-    const uint PWM_TEST_PIN = 3;
+    const uint LED_PIN = 6;
+    const uint PWM_TEST_PIN_1 = 2;
+    const uint PWM_TEST_PIN_2 = 3;
+    const uint PWM_TEST_PIN_3 = 4;
+    const uint PWM_TEST_PIN_4 = 5;
 
 
     gpio_init(LED_PIN);
@@ -31,23 +38,33 @@ int main()
 
 
     PIO pio = pio0;
-    int sm = 0;
     uint offset = pio_add_program(pio, &pwm_program);
-    pwm_program_init(pio, sm, offset, PWM_TEST_PIN);
-    pio_pwm_set_period(pio, sm, (1u << 16) - 1);
+    pwm_program_init(pio, PIO_1_STATE_MACHINE_1, offset, PWM_TEST_PIN_1);
+    pwm_program_init(pio, PIO_1_STATE_MACHINE_2, offset, PWM_TEST_PIN_2);
+    pwm_program_init(pio, PIO_1_STATE_MACHINE_3, offset, PWM_TEST_PIN_3);
+    pwm_program_init(pio, PIO_1_STATE_MACHINE_4, offset, PWM_TEST_PIN_4);
+    pio_pwm_set_period(pio, PIO_1_STATE_MACHINE_1, (1u << 16) - 1);
+    pio_pwm_set_period(pio, PIO_1_STATE_MACHINE_2, (1u << 16) - 1);
+    pio_pwm_set_period(pio, PIO_1_STATE_MACHINE_3, (1u << 16) - 1);
+    pio_pwm_set_period(pio, PIO_1_STATE_MACHINE_4, (1u << 16) - 1);
+
+
     int level = 0;
 
     while (true) 
     {
+        pio_pwm_set_level(pio, PIO_1_STATE_MACHINE_1, 10000 + level*level);
+        pio_pwm_set_level(pio, PIO_1_STATE_MACHINE_2, 20000 + level*level);
+        pio_pwm_set_level(pio, PIO_1_STATE_MACHINE_3, 30000 + level*level);
+        pio_pwm_set_level(pio, PIO_1_STATE_MACHINE_4, 40000 + level*level);
 
-        pio_pwm_set_level(pio, sm, level * level);
         level = (level + 1) % 256;
         sleep_ms(10);
 
-        gpio_put(LED_PIN, 1);
-        sleep_ms(25);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(25);
+        // gpio_put(LED_PIN, 1);
+        // sleep_ms(25);
+        // gpio_put(LED_PIN, 0);
+        // sleep_ms(25);
     }
 
 }
