2d5f35d2f03738e888671296cfd4995d99a0e495
Mateusz Gancarczyk <gancarczyk.mateusz@gmail.com>
2023-03-31 00:27:52 +0200

    Add 24 PWM channel example. Working on PCB and schematic.

---
+++ firmware/imbuelight_stair_controller_fw/src/config.h
@@ -0,0 +1,47 @@
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+
+
+#define PIO_STATE_MACHINE_1   0
+#define PIO_STATE_MACHINE_2   1
+#define PIO_STATE_MACHINE_3   2
+#define PIO_STATE_MACHINE_4   3
+
+
+
+// PINOUT
+// Standard PWM
+#define PWM_CHANNEL_1_PIN       0
+#define PWM_CHANNEL_2_PIN       1
+#define PWM_CHANNEL_3_PIN       2
+#define PWM_CHANNEL_4_PIN       3
+#define PWM_CHANNEL_5_PIN       4
+#define PWM_CHANNEL_6_PIN       5
+#define PWM_CHANNEL_7_PIN       6
+#define PWM_CHANNEL_8_PIN       7
+#define PWM_CHANNEL_9_PIN       8
+#define PWM_CHANNEL_10_PIN      9
+#define PWM_CHANNEL_11_PIN      10
+#define PWM_CHANNEL_12_PIN      11
+#define PWM_CHANNEL_13_PIN      12
+#define PWM_CHANNEL_14_PIN      13
+#define PWM_CHANNEL_15_PIN      14      
+#define PWM_CHANNEL_16_PIN      15
+
+// PWM from PIO 1
+#define PIO_CHANNEL_17_20       pio0
+#define PWM_CHANNEL_17_PIN      16
+#define PWM_CHANNEL_18_PIN      17
+#define PWM_CHANNEL_19_PIN      18
+#define PWM_CHANNEL_20_PIN      19
+
+// PWM from PIO 2
+#define PIO_CHANNEL_21_24       pio1
+#define PWM_CHANNEL_21_PIN      20
+#define PWM_CHANNEL_22_PIN      26
+#define PWM_CHANNEL_23_PIN      22
+#define PWM_CHANNEL_24_PIN      21
+
+
+#endif
--- firmware/imbuelight_stair_controller_fw/src/main.c
+++ firmware/imbuelight_stair_controller_fw/src/main.c
@@ -1,7 +1,9 @@
-
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
+#include "hardware/pwm.h"
 #include "pwm.pio.h"
+#include "pico/cyw43_arch.h"
+#include "config.h"
 
 
 // Write `period` to the input shift register
@@ -18,48 +20,152 @@ void pio_pwm_set_level(PIO pio, uint sm, uint32_t level) {
     pio_sm_put_blocking(pio, sm, level);
 }
 
-#define PIO_1_STATE_MACHINE_1   0
-#define PIO_1_STATE_MACHINE_2   1
-#define PIO_1_STATE_MACHINE_3   2
-#define PIO_1_STATE_MACHINE_4   3
 
 
 int main() 
 {
     const uint LED_PIN = 6;
-    const uint PWM_TEST_PIN_1 = 2;
-    const uint PWM_TEST_PIN_2 = 3;
-    const uint PWM_TEST_PIN_3 = 4;
-    const uint PWM_TEST_PIN_4 = 5;
+
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return -1;
+    }
+
+   // gpio_init(LED_PIN);
+   // gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    gpio_set_function(PWM_CHANNEL_1_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_2_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_3_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_4_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_5_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_6_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_7_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_8_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_9_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_10_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_11_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_12_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_13_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_14_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_15_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_CHANNEL_16_PIN, GPIO_FUNC_PWM);
 
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    uint slice_num_ch1 = pwm_gpio_to_slice_num(PWM_CHANNEL_1_PIN);
+    uint slice_num_ch2 = pwm_gpio_to_slice_num(PWM_CHANNEL_2_PIN);
+    uint slice_num_ch3 = pwm_gpio_to_slice_num(PWM_CHANNEL_3_PIN);
+    uint slice_num_ch4 = pwm_gpio_to_slice_num(PWM_CHANNEL_4_PIN);
+    uint slice_num_ch5 = pwm_gpio_to_slice_num(PWM_CHANNEL_5_PIN);
+    uint slice_num_ch6 = pwm_gpio_to_slice_num(PWM_CHANNEL_6_PIN);
+    uint slice_num_ch7 = pwm_gpio_to_slice_num(PWM_CHANNEL_7_PIN);
+    uint slice_num_ch8 = pwm_gpio_to_slice_num(PWM_CHANNEL_8_PIN);
+    uint slice_num_ch9 = pwm_gpio_to_slice_num(PWM_CHANNEL_9_PIN);
+    uint slice_num_ch10 = pwm_gpio_to_slice_num(PWM_CHANNEL_10_PIN);
+    uint slice_num_ch11 = pwm_gpio_to_slice_num(PWM_CHANNEL_11_PIN);
+    uint slice_num_ch12 = pwm_gpio_to_slice_num(PWM_CHANNEL_12_PIN);
+    uint slice_num_ch13 = pwm_gpio_to_slice_num(PWM_CHANNEL_13_PIN);
+    uint slice_num_ch14 = pwm_gpio_to_slice_num(PWM_CHANNEL_14_PIN);
+    uint slice_num_ch15 = pwm_gpio_to_slice_num(PWM_CHANNEL_15_PIN);
+    uint slice_num_ch16 = pwm_gpio_to_slice_num(PWM_CHANNEL_16_PIN);
 
 
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &pwm_program);
-    pwm_program_init(pio, PIO_1_STATE_MACHINE_1, offset, PWM_TEST_PIN_1);
-    pwm_program_init(pio, PIO_1_STATE_MACHINE_2, offset, PWM_TEST_PIN_2);
-    pwm_program_init(pio, PIO_1_STATE_MACHINE_3, offset, PWM_TEST_PIN_3);
-    pwm_program_init(pio, PIO_1_STATE_MACHINE_4, offset, PWM_TEST_PIN_4);
-    pio_pwm_set_period(pio, PIO_1_STATE_MACHINE_1, (1u << 16) - 1);
-    pio_pwm_set_period(pio, PIO_1_STATE_MACHINE_2, (1u << 16) - 1);
-    pio_pwm_set_period(pio, PIO_1_STATE_MACHINE_3, (1u << 16) - 1);
-    pio_pwm_set_period(pio, PIO_1_STATE_MACHINE_4, (1u << 16) - 1);
+    pwm_set_wrap(slice_num_ch1, 1600);
+    pwm_set_wrap(slice_num_ch2, 1600);
+    pwm_set_wrap(slice_num_ch3, 1600);
+    pwm_set_wrap(slice_num_ch4, 1600);
+    pwm_set_wrap(slice_num_ch5, 1600);
+    pwm_set_wrap(slice_num_ch6, 1600);
+    pwm_set_wrap(slice_num_ch7, 1600);
+    pwm_set_wrap(slice_num_ch8, 1600);
+    pwm_set_wrap(slice_num_ch9, 1600);
+    pwm_set_wrap(slice_num_ch10, 1600);
+    pwm_set_wrap(slice_num_ch11, 1600);
+    pwm_set_wrap(slice_num_ch12, 1600);
+    pwm_set_wrap(slice_num_ch13, 1600);
+    pwm_set_wrap(slice_num_ch14, 1600);
+    pwm_set_wrap(slice_num_ch15, 1600);
+    pwm_set_wrap(slice_num_ch16, 1600);
+
+    pwm_set_chan_level(slice_num_ch1, PWM_CHAN_A, 100);
+    pwm_set_chan_level(slice_num_ch2, PWM_CHAN_B, 200);
+    pwm_set_chan_level(slice_num_ch3, PWM_CHAN_A, 300);
+    pwm_set_chan_level(slice_num_ch4, PWM_CHAN_B, 400);
+    pwm_set_chan_level(slice_num_ch5, PWM_CHAN_A, 500);
+    pwm_set_chan_level(slice_num_ch6, PWM_CHAN_B, 600);
+    pwm_set_chan_level(slice_num_ch7, PWM_CHAN_A, 700);
+    pwm_set_chan_level(slice_num_ch8, PWM_CHAN_B, 800);
+    pwm_set_chan_level(slice_num_ch9, PWM_CHAN_A, 900);
+    pwm_set_chan_level(slice_num_ch10, PWM_CHAN_B, 1000);
+    pwm_set_chan_level(slice_num_ch11, PWM_CHAN_A, 1100);
+    pwm_set_chan_level(slice_num_ch12, PWM_CHAN_B, 1200);
+    pwm_set_chan_level(slice_num_ch13, PWM_CHAN_A, 1300);
+    pwm_set_chan_level(slice_num_ch14, PWM_CHAN_B, 1400);
+    pwm_set_chan_level(slice_num_ch15, PWM_CHAN_A, 1500);
+    pwm_set_chan_level(slice_num_ch16, PWM_CHAN_B, 1580);
+
+    pwm_set_enabled(slice_num_ch1, true);
+    pwm_set_enabled(slice_num_ch2, true);
+    pwm_set_enabled(slice_num_ch3, true);
+    pwm_set_enabled(slice_num_ch4, true);
+    pwm_set_enabled(slice_num_ch5, true);
+    pwm_set_enabled(slice_num_ch6, true);
+    pwm_set_enabled(slice_num_ch7, true);
+    pwm_set_enabled(slice_num_ch8, true);
+    pwm_set_enabled(slice_num_ch9, true);
+    pwm_set_enabled(slice_num_ch10, true);
+    pwm_set_enabled(slice_num_ch11, true);
+    pwm_set_enabled(slice_num_ch12, true);
+    pwm_set_enabled(slice_num_ch13, true);
+    pwm_set_enabled(slice_num_ch14, true);
+    pwm_set_enabled(slice_num_ch15, true);
+    pwm_set_enabled(slice_num_ch16, true);
+    
+
+    uint offset_pio0 = pio_add_program(PIO_CHANNEL_17_20, &pwm_program);
+    pwm_program_init(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_1, offset_pio0, PWM_CHANNEL_17_PIN);
+    pwm_program_init(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_2, offset_pio0, PWM_CHANNEL_18_PIN);
+    pwm_program_init(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_3, offset_pio0, PWM_CHANNEL_19_PIN);
+    pwm_program_init(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_4, offset_pio0, PWM_CHANNEL_20_PIN);
+    pio_pwm_set_period(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_1, (1u << 16) - 1);
+    pio_pwm_set_period(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_2, (1u << 16) - 1);
+    pio_pwm_set_period(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_3, (1u << 16) - 1);
+    pio_pwm_set_period(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_4, (1u << 16) - 1);
+
+
+    uint offset_pio1 = pio_add_program(PIO_CHANNEL_21_24, &pwm_program);
+    pwm_program_init(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_1, offset_pio1, PWM_CHANNEL_21_PIN);
+    pwm_program_init(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_2, offset_pio1, PWM_CHANNEL_22_PIN);
+    pwm_program_init(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_3, offset_pio1, PWM_CHANNEL_23_PIN);
+    pwm_program_init(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_4, offset_pio1, PWM_CHANNEL_24_PIN);
+    pio_pwm_set_period(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_1, (1u << 16) - 1);
+    pio_pwm_set_period(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_2, (1u << 16) - 1);
+    pio_pwm_set_period(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_3, (1u << 16) - 1);
+    pio_pwm_set_period(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_4, (1u << 16) - 1);
 
 
     int level = 0;
 
     while (true) 
     {
-        pio_pwm_set_level(pio, PIO_1_STATE_MACHINE_1, 10000 + level*level);
-        pio_pwm_set_level(pio, PIO_1_STATE_MACHINE_2, 20000 + level*level);
-        pio_pwm_set_level(pio, PIO_1_STATE_MACHINE_3, 30000 + level*level);
-        pio_pwm_set_level(pio, PIO_1_STATE_MACHINE_4, 40000 + level*level);
+        pio_pwm_set_level(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_1, 5000 + level*level);
+        pio_pwm_set_level(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_2, 10000 + level*level);
+        pio_pwm_set_level(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_3, 15000 + level*level);
+        pio_pwm_set_level(PIO_CHANNEL_17_20, PIO_STATE_MACHINE_4, 20000 + level*level);
+
+        pio_pwm_set_level(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_1, 25000 + level*level);
+        pio_pwm_set_level(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_2, 30000 + level*level);
+        pio_pwm_set_level(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_3, 35000 + level*level);
+        pio_pwm_set_level(PIO_CHANNEL_21_24, PIO_STATE_MACHINE_4, 40000 + level*level);
 
         level = (level + 1) % 256;
         sleep_ms(10);
+
+
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
 
         // gpio_put(LED_PIN, 1);
         // sleep_ms(25);
